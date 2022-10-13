4635a938bb512101e122e2b7a48874bb233c312b
ydekor <elefont@yandex.ru>
2022-10-13 21:42:01 +0300

    modified

--- lw1.php
+++ lw1.php
@@ -1,9 +1,10 @@
 <?php
 
-function sumTime($firstTime, $secondTime): void {
+function sumTime($firstTime, $secondTime): void
+{
 
     $first = explode(':', $firstTime);
-    if(!checkCorrectTime($first)) {
+    if (!checkCorrectTime($first)) {
         echo "oshibka";
         return;
     }
@@ -12,37 +13,37 @@ function sumTime($firstTime, $secondTime): void {
     $minute = intval($first[1]) + intval($second[1]);
     $hour = intval($first[0]) + intval($second[0]);
 
-    if($sec >= 60) {
+    if ($sec >= 60) {
         $minute += intval($sec / 60);
         $tempSecs = $sec % 60;
     }
-    if($minute >= 60) {
+    if ($minute >= 60) {
         $hour += intval($sec / 60);
         $minute = $minute % 60;
     }
-    if($hour >= 24) {
+    if ($hour >= 24) {
         $hour = $hour % 24;
     }
-    print 'result: ' . $hour . ':'. $minute . ':' . $sec; 
+    print 'result: ' . $hour . ':' . $minute . ':' . $sec;
 }
 
-function checkCorrectTime($first): bool {
-    if(count ($first) < 3) {
+function checkCorrectTime($first): bool
+{
+    if (count($first) < 3) {
         return false;
     }
-    foreach($first as $element) {
+    foreach ($first as $element) {
         try {
             intval($element);
-        }
-        catch(Exception $e) {
+        } catch (Exception $e) {
             return false;
         }
 
-        if($element < 0) {
+        if ($element < 0) {
             return false;
         }
     }
     return true;
 }
 
-sumTime('10:10:10','10:10:10');
+sumTime('10:10:10', '10:10:10');
