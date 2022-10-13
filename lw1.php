72d4e8eb2df66428a70de777db6a05ac253f6771
ydekor <elefont@yandex.ru>
2022-10-13 23:15:51 +0300

    mod2

--- lw1.php
+++ lw1.php
@@ -1,49 +1,28 @@
 <?php
 
-function sumTime($firstTime, $secondTime): void
+function sumTime(string $firstTime, string $secondTime): string
 {
-    $result = '';
+    $first = [];
+    $second = [];
+    $result = [];
     $first = explode(':', $firstTime);
-    if (!checkCorrectTime($first)) {
-        echo "oshibka";
-        return;
-    }
     $second = explode(':', $secondTime);
-    $sec = intval($first[2]) + intval($second[2]);
-    $minute = intval($first[1]) + intval($second[1]);
-    $hour = intval($first[0]) + intval($second[0]);
-
-    if ($sec >= 60) {
-        $minute += intval($sec / 60);
-        $sec = $sec % 60;
+    $result[0] = (intval($first[0])) + (intval($second[0]));
+    $result[1] = (intval($first[1])) + (intval($second[1]));
+    $result[2] = (intval($first[2])) + (intval($second[2]));
+    if ((intval($result[2])) > 60) {
+        $result[1] = (intval($result[1])) + 1;
+        $result[2] = (intval($result[2])) - 60;
     }
-    if ($minute >= 60) {
-        $hour += intval($sec / 60);
-        $minute = $minute % 60;
+    if ((intval($result[1])) > 60) {
+        $result[0] = (intval($result[0])) + 1;
+        $result[1] = (intval($result[1])) - 60;
     }
-    if ($hour >= 24) {
-        $hour = $hour % 24;
+    if ((intval($result[0])) > 60) {
+        $result[0] = (intval($result[0])) - 24;
     }
-    print 'result: ' . $hour . ':' . $minute . ':' . $sec;
+    print 'result: ' . $result[0] . ':' . $result[1] . ':' . $result[2];
+    return "$result[0]:$result[1]:$result[2]";
 }
 
-function checkCorrectTime($first): bool
-{
-    if (count($first) < 3) {
-        return false;
-    }
-    foreach ($first as $element) {
-        try {
-            intval($element);
-        } catch (Exception $e) {
-            return false;
-        }
-
-        if ($element < 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-sumTime('10:10:10', '10:10:10');
+sumTime('12:10:10', '10:10:10');
